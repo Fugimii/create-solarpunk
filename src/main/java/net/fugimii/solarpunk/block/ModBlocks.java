3b6c76748e0a57bf36dd95ed2ba7b7bb33a97dc5
Fugimii <louisrumbleskins@gmail.com>
2024-09-26 18:13:46 +1000

    Add particles + better sounds to the large solar panel

--- src/main/java/net/fugimii/solarpunk/block/ModBlocks.java
+++ src/main/java/net/fugimii/solarpunk/block/ModBlocks.java
@@ -1,15 +1,18 @@
 package net.fugimii.solarpunk.block;
 
+import com.simibubi.create.AllBlocks;
 import com.tterrag.registrate.util.entry.BlockEntry;
 
 import net.fugimii.solarpunk.SolarpunkMod;
 import net.fugimii.solarpunk.block.solarPanel.LargeSolarPanel;
 
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.material.MapColor;
 
 public class ModBlocks {
 	public static final BlockEntry<LargeSolarPanel> LARGE_SOLAR_PANEL = SolarpunkMod.REGISTRATE.block("large_solar_panel", LargeSolarPanel::new)
-			.initialProperties(() -> Blocks.IRON_BLOCK)
+			.initialProperties(AllBlocks.ANDESITE_CASING)
+			.properties(p -> p.mapColor(MapColor.COLOR_BLUE))
 			.item()
 			.build()
 			.register();
